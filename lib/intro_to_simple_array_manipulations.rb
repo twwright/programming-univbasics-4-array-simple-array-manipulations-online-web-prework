d9712bdc099eb992a2ff417c11c881cb9d2d78a6
Thomas Wright <twwright@gmail.com>
2019-09-24 04:40:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -31,7 +31,7 @@ def using_insert(array, element)
 end
 
 def using_uniq(array)
-  array.uniq 
+  array.uniq
 end
 
 def using_flatten(array)
@@ -45,4 +45,3 @@ end
 def using_delete_at(array, int_to_del)
   array.delete_at(int_to_del)
 end
-
